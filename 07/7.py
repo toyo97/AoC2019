827b937a96ebba25b7090b8b49f2dfce5d46672e
Vittorio Zampinetti <zampinetti@gmail.com>
2019-12-07 18:05:01 +0100

    part 2

--- 07/7.py
+++ 07/7.py
@@ -1,73 +1,95 @@
+"""
+Curious comment: this program works on the input string but not on the test.\
+Or at least it does not give the same result as those given in the\
+problem description
+"""
+
 from itertools import permutations
 
 I = list(map(int, open('7.in').read().strip().split(',')))
 
-sequences = permutations([0,1,2,3,4])
+sequences = permutations([5,6,7,8,9])
 
 res = []
 
 for perm in sequences:
+#    print(f'perm: {perm}')
+
     out = 0
-    print(f'perm: {perm}')
+    end = False
+    idx = [0] * 5
+    PROG = [I[:]] * 5
+    amp = 0
+    first_cycle = 5
+    while not end:
+#        print(f'amp: {amp}')
 
-    for amp in perm:
-        print(f'amp: {amp}')
-        I_copy = I[:]
-        inputs = [amp, out]
-        i = 0
-        print(f'inputs: {inputs}')
+        if first_cycle > 0:
+#            print(f'inputs: {perm[amp]}, {out}')
+            inputs = [perm[amp], out]
+            first_cycle -= 1
+        else:
+#            print(f'inputs: {out}')
+            inputs = [out]
 
-        end = False
-        while not end:
-            instr = str(I_copy[i]).rjust(5, '0')
+        go_next = False
+
+        while (not go_next) and (not end):
+            instr = str(PROG[amp][idx[amp]]).rjust(5, '0')
             opcode = instr[-2:]
-            print(f'opcode: {opcode}')
+
+#            print(f'opcode: {opcode}')
+
         # No-input opcodes
             if opcode in ['03', '04', '99']:
 
                 if opcode == '03':
-                    I_copy[I_copy[i+1]] = inputs.pop(0)
-                    i += 2
+                    PROG[amp][PROG[amp][idx[amp]+1]] = inputs.pop(0)
+                    idx[amp] += 2
 
                 elif opcode == '04':
-                    out = I_copy[I_copy[i+1]]
-                    i += 2
-                    print(f'out: {out}')
+                    out = PROG[amp][PROG[amp][idx[amp]+1]]
+#                    inputs.append(out)
+                    idx[amp] += 2
+                    go_next = True
+#                    print(f'amp {amp} outputs: {out}')
 
                 elif opcode == '99':
                     end = True
 
         # Three-inputs opcodes
             else:
-                a = I_copy[i+1] if int(instr[-3]) else I_copy[I_copy[i+1]]
-                b = I_copy[i+2] if int(instr[-4]) else I_copy[I_copy[i+2]]
+                a = PROG[amp][idx[amp]+1] if int(instr[-3]) else PROG[amp][PROG[amp][idx[amp]+1]]
+                b = PROG[amp][idx[amp]+2] if int(instr[-4]) else PROG[amp][PROG[amp][idx[amp]+2]]
 
                 if opcode == '01':
-                    I_copy[I_copy[i+3]] = a + b
-                    i += 4
+                    PROG[amp][PROG[amp][idx[amp]+3]] = a + b
+                    idx[amp] += 4
 
                 elif opcode == '02':
-                    I_copy[I_copy[i+3]] = a * b
-                    i += 4
+                    PROG[amp][PROG[amp][idx[amp]+3]] = a * b
+                    idx[amp] += 4
 
                 elif opcode == '05':
-                    i = b if a else i+3
+                    idx[amp] = b if a else idx[amp]+3
 
                 elif opcode == '06':
-                    i = b if not a else i+3
+                    idx[amp] = b if not a else idx[amp]+3
 
                 elif opcode == '07':
-                    I_copy[I_copy[i+3]] = 1 if a < b else 0
-                    i += 4
+                    PROG[amp][PROG[amp][idx[amp]+3]] = 1 if a < b else 0
+                    idx[amp] += 4
 
                 elif opcode == '08':
-                    I_copy[I_copy[i+3]] = 1 if a == b else 0
-                    i += 4
+                    PROG[amp][PROG[amp][idx[amp]+3]] = 1 if a == b else 0
+                    idx[amp] += 4
 
                 else:
                     print(f'Unknown opcode {opcode}')
 
-    res.append(out)
+#        output produced, go to go_next amp
+        amp = (amp + 1) % 5
 
+    res.append(out)
 
 print(max(res))

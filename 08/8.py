0ef3c104ea38be4c14faf5b7bb5833cb6d55e3c2
Vittorio Zampinetti <zampinetti@gmail.com>
2019-12-08 12:16:37 +0100

    minor reformatting and sol image upload

--- 08/8.py
+++ 08/8.py
@@ -1,26 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# LAYER DIMENSIONS
 m = 6
 n = 25
 
+# Input reading
 nums = list(map(int, open('8.in').read().strip()))
 
-#print(f'Array length: {len(nums)}')
+# Check if the dimensions fit the input
 assert len(nums) % m*n == 0
 
+# ... and then find the number of layers
 nlayers = len(nums) // (m*n)
 
+
+# ************ PART 1 ***************
+
+# Create 3D tensors
 A = np.array(nums).reshape((nlayers, m, n))
 
-occ0 = np.sum(A == 0, axis=(1,2))
-#print(occ0)
-layX = occ0.argmin() # layer with fewest 0 occurrences
+# Find layer with fewest 0 occurrences
+layX = (np.sum(A == 0, axis=(1,2))).argmin()
 
-print(np.sum(A[layX,:,:] == 1) * np.sum(A[layX,:,:] == 2))
+print(f'Part 1 solution: {np.sum(A[layX,:,:] == 1) * np.sum(A[layX,:,:] == 2)}')
 
+
+# ************ PART 2 ***************
+
+# Solution image initialization
 B = np.zeros((m,n))
 
+# Find foremost visible pixels in tensor and store them in the sol. image
 for i in range(A.shape[1]):
     for j in range(A.shape[2]):
         k = 0
@@ -28,5 +39,6 @@ for i in range(A.shape[1]):
             k += 1
         B[i,j] = A[k,i,j]
 
-plt.imshow(B, interpolation='nearest')
+# Plot the image
+plt.imshow(B)
 plt.show()

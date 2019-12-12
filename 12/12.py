794c36abb5e0e34e9d9b4116603874b00de40999
Vittorio Zampinetti <zampinetti@gmail.com>
2019-12-12 15:53:33 +0100

    part 2 not efficient: not working

--- 12/12.py
+++ 12/12.py
@@ -15,7 +15,7 @@ def compute_energy(P, V):
     kin = np.sum(np.abs(V), axis=1)
     return np.sum(pot*kin)
 
-lines = open('12.in').read().strip().split('\n')
+lines = open('test12_2.in').read().strip().split('\n')
 
 pos = [0,1,2]
 vel = [3,4,5]
@@ -28,8 +28,36 @@ for i,line in enumerate(lines):
         coords[j] = int(coords[j].strip('xyz<>='))
     moons[i, pos] = np.array(coords)
 
-for _ in range(1000):
+energies = compute_energy(moons[:,pos], moons[:,vel])
+visited = moons[:,:,np.newaxis].copy()
+end = False
+
+it = 0
+while not end:
+
     moons[:,vel] += compute_gravity(moons)
     moons[:,pos] += moons[:,vel]
 
-print(compute_energy(moons[:,pos], moons[:,vel]))
+    new_en = compute_energy(moons[:,pos], moons[:,vel])
+    same_en_idx = np.argwhere(energies == new_en)
+    if  same_en_idx.size > 0:
+#        print(len(visited), same_en_idx.size)
+        for i in range(same_en_idx.size):
+            end = np.array_equal(moons, visited[:,:,same_en_idx[i]].reshape(moons.shape))
+            if end:
+                break
+    visited = np.append(visited, moons[:,:,np.newaxis].copy(), axis=2)
+    energies = np.append(energies, new_en)
+    it += 1
+#    if it == 2772:
+#        print(moons)
+#        print(visited[:,:,0])
+#        print(compute_energy(moons[:,pos], moons[:,vel]))
+#        print(energies[-1])
+#        print(np.array_equal(moons, visited[:,:,0]))
+#        print(end)
+#        end = True
+    if it % 10000 == 0:
+        print(it)
+
+print(f'final it: {it}')
